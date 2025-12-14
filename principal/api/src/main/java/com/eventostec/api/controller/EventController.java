951201ff1a380eda3b02d4dcc775026ac02ec03e
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 00:10:03 +0200

    addiction of event controller

--- principal/api/src/main/java/com/eventostec/api/controller/EventController.java
+++ principal/api/src/main/java/com/eventostec/api/controller/EventController.java
@@ -1,4 +1,25 @@
 package com.eventostec.api.controller;
 
+import com.eventostec.api.domain.Event;
+import com.eventostec.api.dto.EventRequestDTO;
+import com.eventostec.api.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+
+    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
+        Event newEvent=this.eventService.createEvent(body);
+        return ResponseEntity.ok(newEvent);
+    }
 }
