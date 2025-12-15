4a4fece07b65bb55c9000b463597cd53a102ac2c
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 16:06:42 +0200

    addiction of pagination to the method that list the events

--- principal/api/src/main/java/com/eventostec/api/service/EventService.java
+++ principal/api/src/main/java/com/eventostec/api/service/EventService.java
@@ -1,9 +1,13 @@
 package com.eventostec.api.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.eventostec.api.dto.EventResponseDTO;
 import com.eventostec.api.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +18,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -74,5 +79,22 @@ public class EventService {
 		fos.close();
 
 		return convFile;
+	}
+
+	public List<EventResponseDTO> getEvents(int page, int size) {
+
+		Pageable pageable= PageRequest.of(page,size);
+		Page<Event> eventPage=this.eventRepository.findAll(pageable);
+		return eventPage.map(event -> new EventResponseDTO(
+				event.getId(),
+				event.getTitle(),
+				event.getDescription(),
+				event.getDate(),
+				"",
+				"",
+				event.getRemote(),
+				event.getEventoUrl(),
+				event.getImgUrl())).stream().toList();
+
 	}
 }
