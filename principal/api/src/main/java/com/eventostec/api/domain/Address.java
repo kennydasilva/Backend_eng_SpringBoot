ea732e8339af519714ed818894967cc44ce4e4f2
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 23:01:43 +0200

    changing of relation

--- principal/api/src/main/java/com/eventostec/api/domain/Address.java
+++ principal/api/src/main/java/com/eventostec/api/domain/Address.java
@@ -4,9 +4,17 @@ import java.util.*;
 import com.eventostec.api.domain.*;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name="address")
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 	
 	@Id
@@ -18,9 +26,9 @@ public class Address {
 	
 	@Column(nullable = false, length = 100)
 	private String uf;
-	
-	@ManyToOne
-	@JoinColumn(name="event_id")
+
+	@OneToOne
+	@JoinColumn(name = "event_id")
 	private Event event;
 
 
