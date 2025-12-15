e0ba1e28e4b14188d96d43960461e29763899d85
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 13:24:37 +0200

    addiction of the method that save the coupon object

--- principal/api/src/main/java/com/eventostec/api/service/CouponService.java
+++ principal/api/src/main/java/com/eventostec/api/service/CouponService.java
@@ -1,7 +1,39 @@
 package com.eventostec.api.service;
 
+import com.eventostec.api.domain.Coupon;
+import com.eventostec.api.domain.Event;
+import com.eventostec.api.dto.CouponRequestDTO;
+import com.eventostec.api.repository.CouponRepository;
+import com.eventostec.api.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CouponService {
+
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponRequestDTO) {
+        Event event=eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+
+        Coupon coupon=new Coupon();
+        coupon.setCode(couponRequestDTO.code());
+        coupon.setDiscount(couponRequestDTO.discount());
+        coupon.setValid(couponRequestDTO.valid());
+        coupon.setEvent(event);
+
+
+        return couponRepository.save(coupon);
+
+    }
 }
