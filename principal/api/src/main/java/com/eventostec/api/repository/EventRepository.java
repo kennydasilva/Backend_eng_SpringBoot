3861d7cd54c0035f5bbc96a270cb86a7346bb066
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 18:34:52 +0200

    addiction of method findUpcomingEvents

--- principal/api/src/main/java/com/eventostec/api/repository/EventRepository.java
+++ principal/api/src/main/java/com/eventostec/api/repository/EventRepository.java
@@ -1,7 +1,13 @@
 package com.eventostec.api.repository;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.eventostec.api.domain.*;
@@ -9,4 +15,7 @@ import com.eventostec.api.domain.*;
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID>{
 
+
+    @Query("SELECT e FROM Event e WHERE e.date>=:currentDate")
+    public Page<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate, Pageable pageable);
 }
