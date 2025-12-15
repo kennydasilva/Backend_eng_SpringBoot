8466f16a1856a45916f26036a74f8ed16e2b6ea8
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 12:22:02 +0200

    addiction of couponController

--- principal/api/src/main/java/com/eventostec/api/controller/CouponController.java
+++ principal/api/src/main/java/com/eventostec/api/controller/CouponController.java
@@ -1,4 +1,11 @@
 package com.eventostec.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/coupon")
 public class CouponController {
+
+
 }
