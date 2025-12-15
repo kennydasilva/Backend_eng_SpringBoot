ba4ae3947ae75071659fa9834e6e948fa3e72d76
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-15 13:23:33 +0200

    set up of controller of coupon

--- principal/api/src/main/java/com/eventostec/api/controller/CouponController.java
+++ principal/api/src/main/java/com/eventostec/api/controller/CouponController.java
@@ -1,11 +1,26 @@
 package com.eventostec.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eventostec.api.domain.Coupon;
+import com.eventostec.api.dto.CouponRequestDTO;
+import com.eventostec.api.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/coupon")
 public class CouponController {
 
+    @Autowired
+    private CouponService couponService;
 
+    @PostMapping("/event/{eventId")
+    public ResponseEntity<Coupon> addCouponsToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO couponRequestDTO){
+
+        Coupon coupon=couponService.addCouponToEvent(eventId, couponRequestDTO);
+        return ResponseEntity.ok(coupon);
+
+    }
 }
